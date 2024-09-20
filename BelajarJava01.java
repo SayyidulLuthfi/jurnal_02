632cf7b22ca8a685e89c63230c0dbd689b97c575
SayyidulLuthfi <sayyidullupi@gmail.com>
2024-09-20 13:54:31 +0700

    jurnal02
    jurnal02

---
+++ BelajarJava01.java
@@ -0,0 +1,7 @@
+class BelajarJava01 {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Luthfi");
+        System.out.println("NIM : 607062400074");
+        System.out.println("Kelas D3IF - 48 - 01");
+    }
+}
